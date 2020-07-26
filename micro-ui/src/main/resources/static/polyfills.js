3f795dba65dc39d14658a4d793062a5a4a9caaf0
Lakshyajit Laxmikant <lakshyajit165@gmail.com>
2020-07-26 18:14:31 +0530

    bug fixed for gateway routing issue

--- micro-ui/src/main/resources/static/polyfills.js
+++ micro-ui/src/main/resources/static/polyfills.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/rakhshanda/delete/microservices_demo/micro-ui/frontend/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/elkay/workspace/microservices/micro-ui/frontend/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
